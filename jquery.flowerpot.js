96d82d4b94678204c3d28252c79d898ce147933d
Matthew Riley MacPherson <matt@lonelyvegan.com>
2009-02-24 02:57:10 +0000

    Using <img> tag (background-images were very problematic in IE :'() Stopped reloading image on browser resize Included ie6 png again

--- jquery.flowerpot.js
+++ jquery.flowerpot.js
@@ -1,11 +1,62 @@
 function Flowerpot_stdObject() {
 	var dom_img = 0;
-	this.ready = false;
+	this.close_img = "images/flowerpot-close.png";
+	if (jQuery.browser.msie == true && jQuery.browser.version < 8)
+		this.close_img = "images/flowerpot-close-ie6.png";
+	this.loading_img = "images/flowerpot-loading.gif";
 	this.overlay = false;
+	this.ready = false;
+}
+
+Flowerpot_stdObject.prototype.do_size = function(object) {
+	var window_height = jQuery(window).height();
+	var window_width = jQuery(window).width();
+	var height = dom_img.height;
+	var width = dom_img.width;
+	
+	if (jQuery.browser.msie == true && jQuery.browser.version < 7) {
+		jQuery("#flowerpotjs-overlay").css('height', jQuery(document).height());
+		jQuery("#flowerpotjs-overlay").css('width', window_width);
+	}
+	
+	var max_height = window_height - window_height / 10;
+	var max_width = window_width - window_width / 10;
+	
+	// Check to make sure the image isn't too big!
+	if (width > max_width) {
+		height = height * (max_width / width);
+		width = max_width;
+		if (height > max_height) {
+			width = width * (max_height / height);
+			height = max_height;
+		}
+	} else if (height > max_height) {
+		width = width * (max_height / height);
+		height = max_height;
+		if (width > max_width) {
+			height = height * (max_width / width);
+			width = max_width;
+		}
+	}
+	
+	jQuery(object).height(height + "px");
+	jQuery(object).width(width + "px");
+	
+	jQuery("#flowerpotjs-contents").css('height', height + "px");
+	if (jQuery.browser.msie == true && jQuery.browser.version < 7) {
+	}
+	else {
+		jQuery("#flowerpotjs-contents").css('margin-top', "-" + (height / 2) + "px");
+		jQuery("#flowerpotjs-contents").css('margin-left', "-" + (width / 2) + "px");
+	}
+	jQuery("#flowerpotjs-contents").css('width', width + "px");
+	
+	jQuery("#flowerpotjs-close").css('bottom', (height + 20) + "px");
+	jQuery("#flowerpotjs-close").css('left', (width - 20) + "px");
 }
 
 Flowerpot_stdObject.prototype.init = function(options) {
-	var flowerpot_html = "<div id=\"flowerpotjs-overlay\" style=\"display: none;\"></div><div id=\"flowerpotjs-loading\" style=\"display: none;\"><span>Loading...</span></div><div id=\"flowerpotjs-contents\" style=\"display: none;\"></div>";
+	var flowerpot_html = "<div id=\"flowerpotjs-overlay\" style=\"display: none;\"></div><div id=\"flowerpotjs-loading\" style=\"display: none;\"><img src=\"" + this.loading_img + "\" alt=\"Loading...\" /></div><div id=\"flowerpotjs-contents\" style=\"display: none;\"></div>";
 	jQuery("body").append(flowerpot_html);
 	jQuery("#flowerpotjs-overlay").css({opacity: 0});
 	
@@ -40,65 +91,11 @@ Flowerpot_stdObject.prototype.init = function(options) {
 	// Resize Events
 	jQuery(window).resize(function(event) {
 		if (the_flowerpot.ready == true) {
-			the_flowerpot.resize("#flowerpotjs-image");
+			the_flowerpot.resize("#flowerpotjs-image", true);
 			event.preventDefault();
 		}
 	});
 };
-
-Flowerpot_stdObject.prototype.resize = function(object) {
-	var window_height = jQuery(window).height();
-	var window_width = jQuery(window).width();
-	
-	dom_img = new Image();
-	jQuery(dom_img).load(function() {
-		var height = dom_img.height;
-		var width = dom_img.width;
-		
-		if (jQuery.browser.msie == true && jQuery.browser.version < 7) {
-			jQuery("#flowerpotjs-overlay").css('height', jQuery(document).height());
-			jQuery("#flowerpotjs-overlay").css('width', window_width);
-		}
-		
-		var max_height = window_height - window_height / 10;
-		var max_width = window_width - window_width / 10;
-		
-		// Check to make sure the image isn't too big!
-		if (width > max_width) {
-			height = height * (max_width / width);
-			width = max_width;
-			if (height > max_height) {
-				width = width * (max_height / height);
-				height = max_height;
-			}
-		} else if (height > max_height) {
-			width = width * (max_height / height);
-			height = max_height;
-			if (width > max_width) {
-				height = height * (max_width / width);
-				width = max_width;
-			}
-		}
-		
-		jQuery(object).height(height + "px");
-		jQuery(object).width(width + "px");
-		
-		jQuery("#flowerpotjs-contents").css('height', height + "px");
-		if (jQuery.browser.msie == true && jQuery.browser.version < 7) {
-		}
-		else {
-			jQuery("#flowerpotjs-contents").css('margin-top', "-" + (height / 2) + "px");
-			jQuery("#flowerpotjs-contents").css('margin-left', "-" + (width / 2) + "px");
-		}
-		jQuery("#flowerpotjs-contents").css('width', width + "px");
-		
-		jQuery("#flowerpotjs-close").css('bottom', (height + 20) + "px");
-		jQuery("#flowerpotjs-close").css('left', (width - 20) + "px");
-		
-		the_flowerpot.ready = true;
-	});
-	dom_img.src = jQuery(object).attr('src');
-}
 
 Flowerpot_stdObject.prototype.hide = function() {
 	jQuery("#flowerpotjs-loading").fadeOut(50);
@@ -111,12 +108,25 @@ Flowerpot_stdObject.prototype.hide = function() {
 	the_flowerpot.ready = false;
 }
 
+Flowerpot_stdObject.prototype.resize = function(object) {
+	the_flowerpot.do_size(object);
+}
+
 Flowerpot_stdObject.prototype.show = function() {
 	jQuery("#flowerpotjs-loading").fadeOut(50);
 	jQuery("#flowerpotjs-close").fadeIn(50);
 	
 	jQuery("#flowerpotjs-contents").fadeIn();
 	this.overlay = true;
+}
+
+Flowerpot_stdObject.prototype.size = function(object) {
+	dom_img = new Image();
+	jQuery(dom_img).load(function() {
+		the_flowerpot.do_size(object);
+		the_flowerpot.ready = true;
+	});
+	dom_img.src = jQuery(object).attr('src');
 }
 
 the_flowerpot = new Flowerpot_stdObject();
@@ -140,7 +150,7 @@ jQuery.fn.flowerpot = function(options) {
 	if (settings['type'] == "gallery") {
 		content = content + "<div id=\"flowerpotjs-prev-link\"><span>Previous Image</span></div><div id=\"flowerpotjs-next-link\"><span>Next Image</span></div>";
 	}
-	content = content + "<a href=\"#close\" id=\"flowerpotjs-close\"><span id=\"flowerpotjs-close-span\"><span style=\"display: none;\">Close</span></span></a>";
+	content = content + "<a href=\"#close\" id=\"flowerpotjs-close\"><img src=\"" + the_flowerpot.close_img + "\" alt=\"Close\" /></a>";
 	
 	// Hide flash from IE < 8 while the overlay is on
 	if ((jQuery.browser.msie == true && jQuery.browser.version < 8) || jQuery.browser.opera) {
@@ -152,12 +162,12 @@ jQuery.fn.flowerpot = function(options) {
 	jQuery("#flowerpotjs-overlay").show().animate({opacity: settings['overlay_opacity']}, 50);
 	jQuery("#flowerpotjs-loading").fadeIn(50);
 	if (jQuery.browser.opera) {
-		the_flowerpot.resize("#flowerpotjs-image");
+		the_flowerpot.size("#flowerpotjs-image");
 		the_flowerpot.show();
 	} else {
 		jQuery("#flowerpotjs-image").load(function callback(event) {
 			if (settings['type'] == "image" || settings['type'] == "gallery") {
-				the_flowerpot.resize("#flowerpotjs-image");
+				the_flowerpot.size("#flowerpotjs-image");
 			}
 			if (!jQuery.support.style) {
 				the_flowerpot.show();
