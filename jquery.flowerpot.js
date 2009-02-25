9e6c3ff9ea8d2bb3a6988e2b5624b9906e1d1767
Matthew Riley MacPherson <matt@lonelyvegan.com>
2009-02-25 05:18:15 +0000

    Image galleries completed Images/look updated Added script info in comments (including license info)

--- jquery.flowerpot.js
+++ jquery.flowerpot.js
@@ -1,10 +1,36 @@
+/**
+ * Flowerpot
+ *
+ * A jQuery plugin to overlay images, inline content, and more.
+ *
+ * @package		Flowerpot
+ * @author		Matthew Riley MacPherson
+ * @copyright	Copyright (c) 2009, Matthew Riley MacPherson
+ * @license		New BSD License (http://www.opensource.org/licenses/bsd-license.php)
+ * @link		http://flowerpot.googlecode.com/
+ * @since		Version 0.1
+ */
+// HTML plants in an overlayed pot!
+
 function Flowerpot_stdObject() {
 	var dom_img = 0;
-	this.close_img = "images/flowerpot-close.png";
-	if (jQuery.browser.msie == true && jQuery.browser.version < 8)
-		this.close_img = "images/flowerpot-close-ie6.png";
-	this.loading_img = "images/flowerpot-loading.gif";
+	var images_dir = "images/";
+	
+	this.close_img = images_dir + "flowerpot-close.png";
+	this.loading_img = images_dir + "flowerpot-loading.gif";
+	this.gallery_prev_img = images_dir + "flowerpot-prev.png";
+	this.gallery_next_img = images_dir + "flowerpot-next.png";
+	// Define IE 6 images, if any, here
+	if (jQuery.browser.msie == true && jQuery.browser.version < 7) {
+		this.close_img = images_dir + "flowerpot-close-ie6.png";
+		this.gallery_prev_img = images_dir + "flowerpot-prev-ie6.png";
+		this.gallery_next_img = images_dir + "flowerpot-next-ie6.png";
+	}
+	
+	this.gallery_index = 0;
+	this.gallery_size = 0;
 	this.overlay = false;
+	this.overlay_opacity = 0.5;
 	this.ready = false;
 }
 
@@ -51,17 +77,20 @@ Flowerpot_stdObject.prototype.do_size = function(object) {
 	}
 	jQuery("#flowerpotjs-contents").css('width', width + "px");
 	
-	jQuery("#flowerpotjs-close").css('bottom', (height + 20) + "px");
-	jQuery("#flowerpotjs-close").css('left', (width - 20) + "px");
+	jQuery("#flowerpotjs-close").css('bottom', (height + 24) + "px");
+	jQuery("#flowerpotjs-close").css('left', (width - 24) + "px");
 }
 
 Flowerpot_stdObject.prototype.init = function(options) {
-	var flowerpot_html = "<div id=\"flowerpotjs-overlay\" style=\"display: none;\"></div><div id=\"flowerpotjs-loading\" style=\"display: none;\"><img src=\"" + this.loading_img + "\" alt=\"Loading...\" /></div><div id=\"flowerpotjs-contents\" style=\"display: none;\"></div>";
+	var flowerpot_html = "<div id=\"flowerpotjs-overlay\" style=\"display: none;\"></div><div id=\"flowerpotjs-loading\" style=\"display: none;\"><img src=\"" + this.loading_img + "\" alt=\"Loading...\" /><span>Loading...</span></div><div id=\"flowerpotjs-contents\" style=\"display: none;\"></div>";
 	jQuery("body").append(flowerpot_html);
 	jQuery("#flowerpotjs-overlay").css({opacity: 0});
 	
+	if (typeof(options) != "object")
+		options = new Array;
+	
 	// Click Events
-	jQuery("#flowerpotjs-close,#flowerpotjs-overlay").live("click", function(event) {
+	jQuery("#flowerpotjs-close,#flowerpotjs-overlay").live("click", function(event) { // Overlay or close link
 		if (event.button == 0 && the_flowerpot.overlay == true) {
 			the_flowerpot.hide();
 			this.blur();
@@ -70,17 +99,58 @@ Flowerpot_stdObject.prototype.init = function(options) {
 	});
 	jQuery(".flowerpot").live("click", function(event) {
 		if (event.button == 0) {
-			jQuery(this).flowerpot(options);
-			this.blur();
-			return false;
+			rel = jQuery(this).attr('rel');
+			if (rel.match(/gallery\[(.[^ ]*)\]/i)) { // Flowerpot image gallery
+				the_flowerpot.gallery_index = jQuery(".flowerpot[rel=" + rel + "]").index(this);
+				the_flowerpot.gallery_size = jQuery(".flowerpot[rel=" + rel + "]").length;
+				jQuery(this).flowerpot({gallery: rel, type: "gallery"});
+				this.blur();
+				return false;
+			} else { // Single Flowerpot image
+				jQuery(this).flowerpot(options);
+				this.blur();
+				return false;
+			}
 		}
 	});
+		// Gallery controls
+		jQuery("#flowerpotjs-prev-link").live("click", function(event) { // Previous image
+			rel = jQuery(this).attr('rel');
+			if (rel.match(/gallery\[(.[^ ]*)\]/i)) {
+				if (event.button == 0) {
+					the_flowerpot.gallery_move("prev");
+					this.blur();
+					return false;
+				}
+			}
+		});
+		jQuery("#flowerpotjs-next-link").live("click", function(event) { // Next image
+			rel = jQuery(this).attr('rel');
+			if (rel.match(/gallery\[(.[^ ]*)\]/i)) {
+				if (event.button == 0) {
+					the_flowerpot.gallery_move("next");
+					this.blur();
+					return false;
+				}
+			}
+		});
 	
 	// Keyboard Events
 	jQuery(document).keydown(function(event) {
 		switch (event.keyCode) {
 			case 27: // Esc key
-				if (the_flowerpot.ready == true) the_flowerpot.hide();
+				if (the_flowerpot.ready == true)
+					the_flowerpot.hide();
+				event.preventDefault();
+				break;
+			case 37: // Left Arrow key
+				if (the_flowerpot.ready == true && the_flowerpot.gallery_size > 0)
+					the_flowerpot.gallery_move("prev");
+				event.preventDefault();
+				break;
+			case 39: // Right Arrow key
+				if (the_flowerpot.ready == true && the_flowerpot.gallery_size > 0)
+					the_flowerpot.gallery_move("next");
 				event.preventDefault();
 				break;
 			default:
@@ -96,6 +166,21 @@ Flowerpot_stdObject.prototype.init = function(options) {
 		}
 	});
 };
+
+Flowerpot_stdObject.prototype.gallery_move = function(index) {
+	jQuery("#flowerpotjs-contents").hide();
+	the_flowerpot.ready = false;
+	if (index == "prev") {
+		the_flowerpot.gallery_index = the_flowerpot.gallery_index - 1;
+		if (the_flowerpot.gallery_index < 0)
+			the_flowerpot.gallery_index = the_flowerpot.gallery_size - 1;
+	} else if (index == "next") {
+		the_flowerpot.gallery_index = the_flowerpot.gallery_index + 1;
+		if (the_flowerpot.gallery_index >= the_flowerpot.gallery_size)
+			the_flowerpot.gallery_index = 0;
+	}
+	jQuery(".flowerpot[rel=" + rel + "]").eq(the_flowerpot.gallery_index).flowerpot({gallery: rel, type: "gallery"});
+}
 
 Flowerpot_stdObject.prototype.hide = function() {
 	jQuery("#flowerpotjs-loading").fadeOut(50);
@@ -133,8 +218,9 @@ the_flowerpot = new Flowerpot_stdObject();
 
 jQuery.fn.flowerpot = function(options) {
 	settings = jQuery.extend({
+		gallery: 0,
 		global: true,
-		overlay_opacity: .5,
+		overlay_opacity: the_flowerpot.overlay_opacity,
 		type: "image"
 	}, options);
 	
@@ -148,7 +234,7 @@ jQuery.fn.flowerpot = function(options) {
 		content = "<img alt=\"" + alt_text + "\" src=\"" + jQuery(this).attr("href") + "\" id=\"flowerpotjs-image\" />";
 	}
 	if (settings['type'] == "gallery") {
-		content = content + "<div id=\"flowerpotjs-prev-link\"><span>Previous Image</span></div><div id=\"flowerpotjs-next-link\"><span>Next Image</span></div>";
+		content = content + "<a href=\"#prev\" id=\"flowerpotjs-prev-link\" class=\"flowerpotjs-gallery-link\" rel=\"" + settings['gallery'] + "\"><img src=\"" + the_flowerpot.gallery_prev_img + "\" alt=\"Previous\" /></a><a href=\"#next\" id=\"flowerpotjs-next-link\" class=\"flowerpotjs-gallery-link\" rel=\"" + settings['gallery'] + "\"><img src=\"" + the_flowerpot.gallery_next_img + "\" alt=\"Next\" /></a>";
 	}
 	content = content + "<a href=\"#close\" id=\"flowerpotjs-close\"><img src=\"" + the_flowerpot.close_img + "\" alt=\"Close\" /></a>";
 	
@@ -158,7 +244,7 @@ jQuery.fn.flowerpot = function(options) {
 		$("#flowerpotjs-contents object,#flowerpotjs-contents embed").css('visibility', "visible");
 	}
 	
-	jQuery("#flowerpotjs-contents").html(content);
+	jQuery("#flowerpotjs-contents").html(content).css({'-moz-border-radius': "2px", '-webkit-border-radius': "1px"});
 	jQuery("#flowerpotjs-overlay").show().animate({opacity: settings['overlay_opacity']}, 50);
 	jQuery("#flowerpotjs-loading").fadeIn(50);
 	if (jQuery.browser.opera) {
