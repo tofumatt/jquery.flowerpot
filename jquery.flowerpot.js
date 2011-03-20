2d9610d77f1f34d5862838c9b1176ac5f2a39c61
Matthew Riley MacPherson <matt@lonelyvegan.com>
2011-03-20 15:16:28 -0300

    Improve basic image support; add description

--- jquery.flowerpot.js
+++ jquery.flowerpot.js
@@ -27,15 +27,22 @@
 		}
 	},
 	animationSpeed = 500, // in ms
+	animationSpeedQuick = 10, // in ms
 	currentFlowerpotElement,
+	// Selectors watched with jQuery().live() -- lightbox is included for compatibility
+	defaultSelector = 'a.flowerpot,a.lightbox',
 	// HTML to use when inserting The Flowerpot into the DOM
 	htmlContainer = '<div id="flowerpot-js-container" />',
 	htmlLoading = '<div id="flowerpot-js-loading" />',
 	htmlOverlay = '<div id="flowerpot-js-overlay" />',
 	htmlOverlayContents = '<div id="flowerpot-js-contents" />',
+	htmlOverlayDescription = '<div id="flowerpot-js-description" />',
 	htmlControlsClose = '<a id="flowerpot-js-controls-close" href="#close">X</a>',
 	// Main image node
 	imageNode,
+	// Proposed max width/height for an overlay
+	initialHeight,
+	initialWidth,
 	// Store library state
 	isLoading = false,
 	// Overlay size and info
@@ -122,8 +129,27 @@
 		maxWidth = windowWidth - windowWidth / 10;
 		
 		// Use the max size allowed if this isn't an image
-		overlayHeight = (type == typeImage && imageNode) ? imageNode.attr('height') : maxHeight;
-		overlayWidth = (type == typeImage && imageNode) ? imageNode.attr('width') : maxWidth;
+		overlayHeight = (type == typeImage && imageNode) ? initialHeight : maxHeight;
+		overlayWidth = (type == typeImage && imageNode) ? initialWidth : maxWidth;
+		
+		/*
+		// Allow sizes to be overriden, if either is set
+		overlayHeight = (size && size.height) ? size.height : height;
+		overlayWidth = (size && size.width) ? size.width : width;
+		
+		// If we're loading an image but only have one dimension,
+		// resize the other dimension appropriately, to maintain
+		// aspect ratio.
+		if (size !== undefined && type == typeImage) {
+			if (imageNode.attr('width') && width && !height) {
+				overlayHeight = imageNode.attr('height') / (imageNode.attr('width') / width);
+			}
+			
+			if (imageNode.attr('height') && height && !width) {
+				overlayWidth = imageNode.attr('width') / (imageNode.attr('height') / height);
+			}
+		}
+		*/
 		
 		// Check to make sure the overlay isn't too big for the
 		// viewport; if it's too tall or too wide, resize it
@@ -160,15 +186,20 @@
 		$(htmlOverlayContents).animate({
 			height: overlayHeight + 'px',
 			left: (($(window).width() / 2) - (overlayWidth / 2)) + 'px',
-			top: (($(window).height() / 2) - (overlayHeight / 2) - 8) + 'px',
+			top: (($(window).height() / 2) - (overlayHeight / 2) - 16) + 'px',
 			width: overlayWidth + 'px'
-		}, animationSpeed / 2);
+		}, animationSpeedQuick);
+		
+		$(htmlOverlayDescription).animate({
+			'margin-left': '-' + (($(htmlOverlayDescription).width() / 2) + 2) + 'px',
+			top: (($(window).height() / 2) + (overlayHeight / 2)) + 'px'
+		}, animationSpeedQuick);
 		
 		$(htmlControlsClose).animate({
 			left: (($(window).width() / 2) - (overlayWidth / 2)) + 'px',
 			'margin-left': (overlayWidth - 8.5) + 'px',
-			top: (($(window).height() / 2) - (overlayHeight / 2) - 8) + 'px'
-		});
+			top: (($(window).height() / 2) - (overlayHeight / 2) - 16) + 'px'
+		}, animationSpeedQuick);
 	},
 	
 	_stopLoadingAnimation = function() {
@@ -183,8 +214,18 @@
 			
 			clearInterval(stateLoadingInterval);
 		}
+		
+		isLoading = false;
 	};
 	
+	/**
+	 * Display an overlay
+	 *
+	 * TODO: Document more...
+	 *
+	 * @return {jQuery} Return jQuery to allow method chaining
+	 * @author Matthew Riley MacPherson matt@servicepoint.ca
+	 */
 	$.fn.flowerpot = function(args) {
 		// Don't operate on selectors that don't return elements.
 		if (!$(this).length) {
@@ -200,6 +241,16 @@
 		// Reference the current Flowerpot'd element.
 		currentFlowerpotElement = this;
 		
+		// Don't try to load an overlay if we're currently processing one.
+		if (isLoading) {
+			return;
+		}
+		
+		// Now we're loading the overlay; blur the activated element to prevent
+		// outline styles and extra events that can fire from a focus state.
+		isLoading = true;
+		$(this).blur();
+		
 		// Load up the "loading" overlay.
 		$.flowerpot.loading();
 		
@@ -207,48 +258,60 @@
 		$(htmlOverlayContents).html(imageNode);
 		
 		$(imageNode).load(function(event) {
+			initialHeight = imageNode.attr('height'),
+			initialWidth = imageNode.attr('width');
 			$.flowerpot.show();
 		});
+		
+		if ($(this).attr('title')) {
+			$(htmlOverlayDescription).html($(this).attr('title'));
+		}
 		
 		// Return this object so we can jQuery method chain.
 		return this;
 	};
 	
-	$.flowerpot = function(args) {
-		
+	$.flowerpot = function(selector) {
+		$(selector).live('click', function(e) {
+			e.preventDefault();
+			
+			$(this).flowerpot();
+		});
 	};
 	
 	$.flowerpot.hide = function() {
-		$(htmlControlsClose).hide();
-		$(htmlOverlayContents).animate({
-			height: ($(window).height() - 10) + 'px',
-			left: '10px',
-			opacity: 0,
-			top: '10px',
-			width: ($(window).width() - 10) + 'px'
-		}, animationSpeed);
+		if (!isLoading) {
+			$.each([htmlOverlay, htmlOverlayContents, htmlOverlayDescription, htmlControlsClose], function(i, element) {
+				$(element).animate({opacity: 0}, animationSpeed / 2);
+			});
+		} else {
+			_stopLoadingAnimation();
+		}
 		
 		setTimeout(function() {
-			$(htmlOverlayContents).css({opacity: 1});
-			$.each([htmlOverlay, htmlOverlayContents], function(i, element) {
+			$.each([htmlOverlay, htmlOverlayContents, htmlOverlayDescription, htmlControlsClose], function(i, element) {
 				$(element).hide();
 			});
+			$(htmlOverlayContents).css({opacity: 1});
+			$(htmlOverlayDescription).css({opacity: 0.85})
+			$(htmlControlsClose).css({opacity: 1});
 		}, animationSpeed * 2);
 	};
 	
 	$.flowerpot.init = function() {
 		// Insert the basic Flowerpot HTML into the DOM.
-		$('body').append($(htmlContainer).append(htmlOverlay, htmlLoading, htmlOverlayContents, htmlControlsClose));
+		$('body').append($(htmlContainer).append(htmlOverlay, htmlLoading, htmlOverlayContents, htmlOverlayDescription, htmlControlsClose));
 		
 		// Assign the variables to actual DOM elements rather than using selectors.
 		htmlContainer = $('#flowerpot-js-container'),
 		htmlLoading = $('#flowerpot-js-loading'),
 		htmlOverlay = $('#flowerpot-js-overlay'),
 		htmlOverlayContents = $('#flowerpot-js-contents'),
+		htmlOverlayDescription = $('#flowerpot-js-description'),
 		htmlControlsClose = $('#flowerpot-js-controls-close');
 		
-		// Assign a Flowerpot event to every <a> element with the class "flowerpot".
-		$('a.flowerpot').live('click', function(e) {
+		// Assign a Flowerpot event to every <a> element with the class "flowerpot" and "lightbox".
+		$(defaultSelector).live('click', function(e) {
 			e.preventDefault();
 			
 			$(this).flowerpot();
@@ -260,10 +323,16 @@
 			
 			$.flowerpot.hide();
 		});
+		
+		// Attach an event fired on event resize to make sure the overlay doesn't exceed the
+		// viewport's height/width.
+		$(window).resize(function(e) {
+			_resize();
+		});
 	};
 	
 	$.flowerpot.loading = function() {
-		$(htmlOverlay).fadeIn(animationSpeed);
+		$(htmlOverlay).show();
 		_loadingAnimation();
 	};
 	
@@ -274,8 +343,12 @@
 	$.flowerpot.show = function() {
 		_resize();
 		
-		$(htmlOverlayContents).fadeIn(animationSpeed);
-		$(htmlControlsClose).fadeIn(animationSpeed);
+		$(htmlOverlay).animate({opacity: 0.99}, animationSpeed)
+		$.each([htmlOverlayContents, htmlControlsClose, htmlOverlayDescription], function(i, element) {
+			$(element).fadeIn(animationSpeed);
+		});
+		
+		htmlOverlayDescription
 		_stopLoadingAnimation();
 	};
 	
